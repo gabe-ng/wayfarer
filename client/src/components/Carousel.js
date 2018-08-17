72e1caf4f82d7c9991d47fa8734a8a098a159eea
Gabriel Ng <gabrielng15@gmail.com>
2018-08-17 11:50:56 -0700

    carousel functioning

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,37 +1,33 @@
 import React, { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import BigBen from "../assets/images/BigBen.jpeg";
-import GoldenGate from "../assets/images/SanFrancisco.jpg";
-import BurjAlArab from "../assets/images/Dubai.jpg";
-import OperaHouse from "../assets/images/Sydney.jpg";
+import BigBen from "../images/BigBen.jpg";
+import GoldenGate from "../images/SanFrancisco.jpg";
+import BurjAlArab from "../images/Dubai.jpg";
+import OperaHouse from "../images/Sydney.jpg";
 
 class HomeCarousel extends Component {
   render() {
     return (
-      <div className="carousel-container">
-        <Carousel>
-          <div>
-            <img src={BigBen} alt="Big Ben" />
-            <p className="legend">London</p>
-          </div>
-          <div>
-            <img
-              src={GoldenGate}
-              alt="Golden Gate Bridge"
-            />
-            <p className="legend">San Francisco</p>
-          </div>
-          <div>
-            <img src={BurjAlArab} alt="Burj Al Arab" />
-            <p className="legend">Dubai</p>
-          </div>
-          <div>
-            <img src={OperaHouse} alt="Sydney Opera House" />
-            <p className="legend">Sydney</p>
-          </div>
-        </Carousel>
-      </div>
+      <Carousel className="carousel-container">
+        <div>
+          <img src={BigBen} alt="Big Ben" />
+          <p className="legend">London</p>
+        </div>
+        <div>
+          <img src={GoldenGate} alt="Golden Gate Bridge" />
+          <p className="legend">San Francisco</p>
+        </div>
+        <div>
+          <img src={BurjAlArab} alt="Burj Al Arab" />
+          <p className="legend">Dubai</p>
+        </div>
+        <div>
+          <img src={OperaHouse} alt="Sydney Opera House" />
+          <p className="legend">Sydney</p>
+        </div>
+      </Carousel>
     );
   }
 }
