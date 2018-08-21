7798d8620fa7e118e6901942c5de8c4afc5d61e4
Gabriel Ng <gabrielng15@gmail.com>
2018-08-21 12:56:08 -0700

    updated seed data

--- server/seed.js
+++ server/seed.js
@@ -13,41 +13,79 @@ let userList = [
 
 let postList = [
   {
-    title: "My first post!",
-    body: "This is a great test post, don't you think?",
-    city: "San Francisco",
-    image: "None at the moment",
-  },
-  {
-    title: "My second post!",
-    body: "This is a great test post, don't you think?",
+    title: "Nice getaway",
+    body:
+      "This opera building is beautiful. I have an extra ticket, whaddaya say @Bruce?",
     city: "Sydney",
     image: "None at the moment",
+    username: "Black Widow"
   },
   {
-    title: "My second Sydney post!",
-    body: "This is a great test post, don't you think?",
+    title: "Uhh..",
+    body: "Uh I don't think we should Natasha. Hold on a sec, I think I dropped my wallet by our jet. I'll brb...",
     city: "Sydney",
     image: "None at the moment",
+    username: "Bruce Banner"
+  },
+  {
+    title: "WAKANDA FOREVA",
+    body:
+      "Wakanda must open trade with these people and I will construct my new summer home here.",
+    city: "Dubai",
+    image: "None at the moment",
+    username: "Black Panther"
   },
   {
     title: "My thirs post!",
     body: "This is a great test post, don't you think?",
     city: "Dubai",
     image: "None at the moment",
+    username: "Captain America"
   },
   {
-    title: "WOw",
-    body: "This is a great test post, don't you think?",
+    title: "Too many cars on this bridge.",
+    body:
+      "The traffic on this bridge is too much. It will eventually collapse from the ever growing car population. I will use the infinity stones to wipe out half the automobiles in San Francisco.",
     city: "San Francisco",
     image: "None at the moment",
+    username: "Thanos"
   },
   {
-    title: "hello posting",
-    body: "This is a great test post, don't you think?",
+    title: "Should we intervene?",
+    body: "Hey Avengers... I kind of agree with Thanos. SF Traffic sucks.",
+    city: "San Francisco",
+    image: "None at the moment",
+    username: "Iron Man"
+  },
+  {
+    title: "I am Groot",
+    body: "I am Groot, I am Groot? I am Groot",
     city: "London",
     image: "None at the moment",
+    username: "Groot"
   },
+  {
+    title: "No Groot",
+    body: "No Groot, you cannot play video games in the palace.",
+    city: "London",
+    image: "None at the moment",
+    username: "Rocket"
+  },
+  {
+    title: "Colossus",
+    body:
+      "*In Russian accent* Oh, this is my hometown. I love the shishkabobs.",
+    city: "Moscow",
+    image: "None at the moment",
+    username: "Deadpool"
+  },
+  {
+    title: "Not Funny.",
+    body: "Not funny Wade. Do not mock my motherland.",
+    city: "Moscow",
+    image: "None at the moment",
+    username: "Colossus"
+  }
 ];
 
 db.User.remove({}, (err, users) => {
